91c201b5a4a89cd0b8a5956af06c1bea67ce409c
merasifa <merasifa@gmail.com>
2023-10-09 14:06:16 +0700

    variasi 3

--- ForVar328.java
+++ ForVar328.java
@@ -2,5 +2,20 @@ import java.util .Scanner;
 
 
 public class ForVar328 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int bil, n;
+        boolean berhenti = false;
+        for (n = 0; !berhenti; n++) {
+            System.out.println("Masukkan bilangan:  ");
+            bil = sc.nextInt();
+            System.out.println("Bilangan yang Anda masukkan: " + bil);
+            if (bil < n) {
+                berhenti = true;
+            }
+        }
+        System.out.println("Program berakhir");
+    }
+
 }
