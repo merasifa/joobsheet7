9a2d8a1ebf948dfb9a79e094ad9ad4e1e699cb38
merasifa <merasifa@gmail.com>
2023-10-09 12:44:48 +0700

    for java

---
+++ WhileKelipatan28.java
@@ -0,0 +1,13 @@
+import java.util .Scanner;
+
+public class WhileKelipatan28 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in); // deklarasi scanner dengan nama scan
+        int total, counter, jumlahKelipatan;
+        double rataRata; // deklarasi variabel 
+        total = 0; // inisialisasi variabel dengan  niali 0
+        counter =0; // inisialisasi variabel dengan  nilai 0
+        jumlahKelipatan = 0;
+
+    }
+}
