5b8f26d0ea17d324b02175f46ce63f9015b02bd5
merasifa <merasifa@gmail.com>
2023-10-09 14:33:02 +0700

    while kelipatan

--- WhileKelipatan28.java
+++ WhileKelipatan28.java
@@ -3,20 +3,36 @@ import java.util .Scanner;
 public class WhileKelipatan28 {
     public static void main(String[] args) {
         Scanner scan = new Scanner (System.in); // deklarasi scanner dengan nama scan
-        int total, counter, jumlahKelipatan;
-        double rataRata; // deklarasi variabel 
-        total = 0; // inisialisasi variabel dengan  niali 0
-        counter =0; // inisialisasi variabel dengan  nilai 0
-        jumlahKelipatan = 0;
-        int i = 1;
+        int total=0, counter= 0, jumlahKelipatan= 0, kelipatan;
+        double rataRata;
 
+        System.out.println("Masukkan bilangan kelipatan (1-9) : ");
+        kelipatan = scan.nextInt();
+
+        int i = 1;
         while (i <= 50) { // Kondisi
             if (i % kelipatan == 0) {
                 total += i; // Eksekusi jika i adalah kelipatan
+                counter++;
             }
-            counter++; // Selalu tambahkan counter
-            i++; // Pembaruan nilai i
+             i++; // Pembaruan nilai i
         }
+
+        i = 1;
+        while (i <= 50) { // Kondisi
+            if (i % kelipatan == 0) {
+                jumlahKelipatan++; 
+            }
+            i++; // Pembaruan nilai i
+
+        }
+           rataRata = (double) total / jumlahKelipatan ;
+        
+
+        System.out.printf("Banyaknya %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
+        System.out.println("Rata rata adalah " + rataRata);
     }
 
 }
+
